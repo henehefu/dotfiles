26f845f7a62e0338e9e080c277f69c1779e42be9
jdoi <jdoi@jme.jp>
2013-04-25 13:15:10 +0900

    melpa 設定 helm追加 フォント設定変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,12 +8,18 @@
 (column-number-mode t)
 (setq-default tab-width 4)
 
+(when (require 'package nil t)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (package-initialize))
+
 ;; Emacs 23 より前のバージョンを利用している方は
 ;; user-emacs-directory変数が未定義のため次の設定を追加
 (when (< emacs-major-version 23)
   (defvar user-emacs-directory "~/.emacs.d/"))
 
 ;; load-path を追加する関数を定義
+;; なんか動いてなくね？(´・ω・`) 
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
@@ -24,37 +30,42 @@
             (normal-top-level-add-subdirs-to-load-path))))))
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-(add-to-load-path "lib")
+(add-to-load-path "~/.emacs.d/lib/")
 
 
 ; フォント設定
 (when window-system
- (set-face-attribute 'default nil
-                     :family "monaco"
-                     :height 180)
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0208
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+;;; フォントセットを作る
+  (let* ((fontset-name "myfonts") ; フォントセットの名前
+         (size 18) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+         (asciifont "Menlo") ; ASCIIフォント
+         (jpfont "Hiragino Maru Gothic ProN") ; 日本語フォント
+         (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
+         (fontspec (font-spec :family asciifont))
+         (jp-fontspec (font-spec :family jpfont)) 
+         (fsn (create-fontset-from-ascii-font font nil fontset-name)))
+    (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
+    (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
+    (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+    (set-fontset-font fsn '(#x0080 . #x024F) fontspec) ; 分音符付きラテン
+    (set-fontset-font fsn '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
+    )
 
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'japanese-jisx0212
-  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+;;; デフォルトのフレームパラメータでフォントセットを指定
+  (add-to-list 'default-frame-alist '(font . "fontset-myfonts"))
 
- (set-fontset-font
-  (frame-parameter nil 'font)
-  'mule-unicode-0100-24ff
-  '("monaco" . "iso10646-1"))
-
- (setq face-font-rescale-alist
-      '(("^-apple-hiragino.*" . 1.2)
-        (".*osaka-bold.*" . 1.2)
-        (".*osaka-medium.*" . 1.2)
-        (".*courier-bold-.*-mac-roman" . 1.0)
-        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-        (".*monaco-bold-.*-mac-roman" . 0.9)
-        ("-cdac$" . 1.3))))
+;;; フォントサイズの比を設定
+  (dolist (elt '(("^-apple-hiragino.*" . 1.2)
+                 (".*osaka-bold.*" . 1.2)
+                 (".*osaka-medium.*" . 1.2)
+                 (".*courier-bold-.*-mac-roman" . 1.0)
+                 (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+                 (".*monaco-bold-.*-mac-roman" . 0.9)))
+    (add-to-list 'face-font-rescale-alist elt))
+  
+;;; デフォルトフェイスにフォントセットを設定
+;;; (これは起動時に default-frame-alist に従ったフレームが作成されない現象への対処)
+  (set-face-font 'default "fontset-myfonts"))
 
 (setq ring-bell-function 'ignore)
 
@@ -160,51 +171,24 @@
 ;; coffee → js用 auto-revert-mode
 (setq auto-revert-interval 1)
 
+
 ;;(add-to-list 'load-path "~/.emacs.d/auto-install/") 
 ;; auto-install
-(when (require 'auto-install nil t)
-  ;(auto-install-update-emacswiki-package-name t)
-  ;(auto-install-compatibility-setup)
+;; (when (require 'auto-install nil t)
+;;   ;(auto-install-update-emacswiki-package-name t)
+;;   ;(auto-install-compatibility-setup)
+;;   )
+
+;; helm でc-hできるようにする
+(eval-after-load 'helm
+  '(progn
+     (define-key helm-map (kbd "C-h") 'delete-backward-char)))
+(eval-after-load 'helm-files
+  '(progn
+     (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)))
+
+(when (require 'helm-config nil t)
+  (global-set-key (kbd "C-c h") 'helm-mini)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
   )
-
-;; anything
-(when (require 'anything-hoge nil t)
-  (setq
-   ;; 候補を表示するまでの時間
-   anything-idle-delay 0.3
-   
-   ;; タイプして再描写するまでの時間
-   anything-input-idle-delay 0.2
-   
-   ;; 候補の表示件数
-   anything-condidate-number-limit 100
-
-   ;; 候補が多いときに体感速度を速くする
-   anything-quick-update t
-
-   ;; 候補のショートカットをアルファベットに
-   anything-enable-shortcuts 'alphabet)
-
-  (when (require 'anything-config nil t)
-    ;; root権限でのアクションを実行するときのコマンド
-    (setq anything-su-or-sudo "sudo"))
-
-  (require 'anything-match-plugin nil t)
-
-  (when (and (executable-find "cmigemo")
-             (require 'migemo nil t))
-    (require 'anything-migemo nil t))
-
-  (when (require 'anything-complete nil t)
-    ;; lispシンボルの補完候補の再検索時間
-    (anything-lispcomplete-symbol-set-timer 150))
-
-  (require 'anything-show-completion nil t)
-
-  (when (require 'auto-install nil t)
-    (require 'anything-auto-install nil t))
-
-  (when (require 'descbinds-anything nil t)
-    ;; describe-bindingsをanythingに置き換える
-    (descbinds-anything-install)))
 
